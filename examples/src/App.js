026519deb4f00245e3e42524226809d7bb93f65a
Derek Mullins <drkmullins@gmail.com>
2017-11-11 11:44:04 -0500

    react-visjs-timeline fun

--- examples/src/App.js
+++ examples/src/App.js
@@ -3,51 +3,43 @@ import Timeline from 'react-visjs-timeline'
 import moment from 'moment'
 import './App.css'
 
-const basicExample = {
-  options: {
-    start: '2014-04-10',
-    end: '2014-04-30',
-  },
-  items: [
-    {id: 1, content: 'item 1', start: '2014-04-20'},
-    {id: 2, content: 'item 2', start: '2014-04-14'},
-    {id: 3, content: 'item 3', start: '2014-04-18'},
-    {id: 4, content: 'item 4', start: '2014-04-16', end: '2014-04-19'},
-    {id: 5, content: 'item 5', start: '2014-04-25'},
-    {id: 6, content: 'item 6', start: '2014-04-27', type: 'point'}
-  ],
-}
-
 const groupsExample = {
   groups: [],
   items: [],
   options: {
-    groupOrder: 'content'  // groupOrder can be a property name or a sorting function
+    groupOrder: 'content',  // groupOrder can be a property name or a sorting function
+    orientation: 'top',
+    margin: 30,
   },
-}
+};
 
-const now = moment().minutes(0).seconds(0).milliseconds(0)
-const groupCount = 3
-const itemCount = 20
+const now = moment().minutes(0).seconds(0).milliseconds(0);
+const tailCount = 4;
+const itemCount = 10;
 
-// create a data set with groups
-const names = ['John', 'Alston', 'Lee', 'Grant']
-for (let g = 0; g < groupCount; g++) {
-  groupsExample.groups.push({ id: g, content: names[g] })
-}
+groupsExample.groups.push(
+  { id: 0, content: 'N310QS' },
+  { id: 1, content: 'N305QS' },
+  { id: 2, content: 'N303QS' },
+  { id: 3, content: 'N201QS' },
+  { id: 4, content: 'N512QS', nestedGroups: [0, 1] },
+  { id: 5, content: 'N515QS', nestedGroups: [2] },
+  { id: 6, content: 'N519QS', nestedGroups: [3] },
+  { id: 7, content: 'N530QS' },  
+)
 
 // create a dataset with items
 for (let i = 0; i < itemCount; i++) {
-  const start = now.clone().add(Math.random() * 200, 'hours')
-  const group = Math.floor(Math.random() * groupCount)
+  const start = now.clone().add(Math.random() * 48, 'hours');
+  const end = start.clone().add(Math.random() * 5 + 1, 'hours');
+  const group = Math.floor(Math.random() * tailCount);
   groupsExample.items.push({
     id: i,
-    group: group,
-    content: 'item ' + i +
-      ' <span style="color:#97B0F8">(' + names[group] + ')</span>',
-    start: start,
-    type: 'box'
-  })
+    group,
+    start,
+    end,
+    type: 'range'
+  });
 }
 
 class App extends Component {
@@ -62,20 +54,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p className="header">A basic timeline. You can move and zoom the timeline, and select items.</p>
-        <Timeline
-          {...basicExample}
-        />
-        <p className="header">This example demonstrate using groups. Note that a DataSet is used for both
-          items and groups, allowing to dynamically add, update or remove both items
-          and groups via the DataSet.</p>
         <Timeline
           {...groupsExample}
           clickHandler={this.clickHandler.bind(this)}
           selection={this.state.selectedIds}
         />
       </div>
-    )
+    );
   }
 
   clickHandler(props) {
